8711bc2e5986a9c260a0cba87dbf784070347a94
songshuan <117974929+songshuan@users.noreply.github.com>
2022-11-11 16:28:33 +0800

    Add files via upload

---
+++ Solution/js/index.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+	var i = 0;
+	$(window).resize(function(){
+		if($(window).width() < 768){
+			$(".bot-top li:odd").addClass("tran")
+			$(".bot-top li:even").addClass("tran")
+		}else{
+			$(".bot-top li:odd").removeClass("tran")
+			$(".bot-top li:even").removeClass("tran")
+		}
+	})
+	if($(window).width() > 768){
+		$(document).on("mousewheel DOMMouseScroll", function(e) {
+			var delta = (e.originalEvent.wheelDelta && (e.originalEvent.wheelDelta > 0 ? 1 : -1)) || // chrome & ie
+				(e.originalEvent.detail && (e.originalEvent.detail > 0 ? -1 : 1));
+			if (delta > 0) { //向上滚动
+				var num = 2;
+				$(".bot-top li:odd").css("transform", "translateY(" + (num + i--) + "px)")
+				$(".bot-top li:even").css("transform", "translateY(-" + (num + i--) + "px)")
+				console.log(i)
+				if (i <= 0) {
+					i = 0;
+					$(".bot-top li:odd").css("transform", "translateY(0px)")
+					$(".bot-top li:even").css("transform", "translateY(-0px)")
+				}
+			}
+			if (delta < 0) { //向下滚动
+				var num = 2;
+				$(".bot-top li:odd").css("transform", "translateY(" + (num + i++) + "px)")
+				$(".bot-top li:even").css("transform", "translateY(-" + (num + i++) + "px)")
+				if (i >= 88) {
+					i = 88;
+					$(".bot-top li:odd").css("transform", "translateY(88px)")
+					$(".bot-top li:even").css("transform", "translateY(-88px)")
+				}
+			}
+		})
+	}
+})
