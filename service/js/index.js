d27a430a6573119d7d608f7c4cc24665867bdfb4
songshuan <117974929+songshuan@users.noreply.github.com>
2022-11-11 16:52:23 +0800

    Add files via upload

---
+++ service/js/index.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+	$("nav").css("left", "0px");
+	$("#sub").click(function() {
+		// 验证姓名
+		if ($("input[name='name']").val().length == "") {
+			window.confirm("请输入你的姓名!", function() {
+				$("input[name='name']").focus();
+			});
+			return false;
+		}
+		//验证电话
+		if($("input[name='phone']").val().length == ""){
+			window.confirm("请输入你的电话号!",function(){
+				$("input[name='phone']").focus();
+			})
+			return false;
+		}
+		if($("input[name='phone']").val().length < 11 || $("input[name='phone']").val().length > 11){
+			window.confirm("请输入正确的电话号!",function(){
+				$("input[name='phone']").focus();
+			})
+			return false;
+		}
+		// 验证邮箱
+		if($("input[name='mail']")){
+			if($(this).val() == "" || ($(this).val() != "" && !/^[\w\-\.]+@[\w\-\.]+(\.\w+)+$/.test($(this).val()))){
+				window.confirm('请输入正确的邮箱地址!')
+				return false;
+			}
+		}
+		// 验证留言
+		if($("input[name='message']").val() == ""){
+			window.confirm("请输入留言内容!")
+			return false;
+		}
+		// 验证码
+		if($("input[name='exam']").val().length < 4 || $("input[name='exam']").val() == ""){
+			window.confirm("请输入正确的验证码!")
+		}
+	})
+})
---
+++ service/js/public.js
@@ -0,0 +1,158 @@
+$(document).ready(function() {
+	// nav
+	$("nav").css("left", "0px");
+	//fixed 下拉导航
+	$(".nav-span1").click(function() {
+		$("#fixed_body").css("left", "0px")
+	})
+	$(".close").click(function() {
+		$(this).parents("#fixed_body").css("left", "-200%")
+	})
+	// 下拉导航 hover图片
+	$("#fixed_body>ul>li>img").css({
+		"transform": "translate(-50%,-50%)scale(1.1)",
+		"opacity": "0"
+	})
+	$("#fixed_body>ul>li>a").hover(function() {
+		$(this).siblings("img").css({
+			"transform": "translate(-50%,-50%)scale(1)",
+			"opacity": "0.1"
+		})
+	}, function() {
+		$(this).siblings("img").css({
+			"transform": "translate(-50%,-50%)scale(1.1)",
+			"opacity": "0"
+		})
+	})
+	// 767px 下拉导航
+	$(".phone-nav>ul>li>h4").click(function() {
+		$(this).siblings("ul").stop(true).slideToggle(400).parent().siblings().find("ul").slideUp(400);
+		$(this).find("img:eq(0)").stop(true).toggle().parent().parent().siblings().find("h4>img:eq(0)").show();
+		$(this).parent().not(":has(ul)").siblings().find("ul").stop(true).slideUp(400).end()
+			.siblings().find("h4>img:eq(0)").show();
+	})
+	//下拉导航 执行动画 添加
+	$("#fixed_body>ul>li>a").css("transform", "translateY(-50px)");
+	$(".fixed-bottom>span").css("transform", "translateY(20px)")
+	$(".fixed-bottom>i").css("transform", "translateY(20px)")
+	$(".nav-span1").click(function() {
+		$("#fixed_body>ul>li>a").css("transform", "translateY(0px)");
+		$(".fixed-bottom>span").css("transform", "translateY(0px)")
+		$(".fixed-bottom>i").css("transform", "translateY(0px)")
+	})
+	//下拉导航 执行动画 删除
+	$(".close").click(function() {
+		$("#fixed_body>ul>li>a").css("transform", "translateY(-50px)");
+		$(".fixed-bottom>span").css("transform", "translateY(20px)")
+		$(".fixed-bottom>i").css("transform", "translateY(20px)")
+	})
+	// 搜索导航
+	$(".fixed-search>.search>.share-wrap>span").css("transform", "translateY(30px)")
+	$(".fixed-search>.search>.share-wrap>i").css("transform", "translateY(30px)")
+	$(".nav-i").click(function() {
+		$(".fixed-search").css("left", "0%")
+		$(".fixed-search>.search>.share-wrap>span").css("transform", "translateY(0px)")
+		$(".fixed-search>.search>.share-wrap>i").css("transform", "translateY(0px)")
+	})
+	$(".Carousel-img").click(function() {
+		$(".fixed-search").css("left", "-180%")
+		$(".fixed-search>.search>.share-wrap>span").css("transform", "translateY(30px)")
+		$(".fixed-search>.search>.share-wrap>i").css("transform", "translateY(30px)")
+	})
+	$(".fixed-search").find(".txt-close").click(function() {
+		$(this).parents(".fixed-search").css("left", "-100%")
+	})
+
+	// 990 头部导航 淡入淡出
+	$(".navigation-none").hide();
+	$(window).resize(function() {
+		if ($(this).width() > 1200) {
+			$(".navigation-none").fadeOut(300)
+		} else {
+			$(".navigation-none").fadeIn(300);
+		}
+	})
+	
+	$(window).scroll(function(){
+		// 右部固定列表
+		if($(this).scrollTop() > 100){
+			$(".right-nav").css("right","0px")
+		}else{
+			$(".right-nav").css("right","-98px")
+		}
+	})
+	// 点击回顶部
+	$("#return_Top").click(function(){
+		$("html").animate({scrollTop:"0"},800);
+	})
+	
+	//箭头抖动
+	$(".animate>a").hover(function(){
+		$(".shake",this).animate({paddingLeft:"18px"},100,function(){
+			$(".shake",this).animate({paddingLeft:"8px"},100)
+		})
+		$(".shake",this).animate({paddingLeft:"8px"},100,function(){
+			$(".shake",this).animate({paddingLeft:"18px"},100)
+		})
+		$(".shake",this).animate({paddingLeft:"18px"},100,function(){
+			$(".shake",this).animate({paddingLeft:"8px"},100)
+		})
+		$(".shake",this).animate({paddingLeft:"8px"},100,function(){
+			$(".shake",this).animate({paddingLeft:"18px"},100)
+		})
+		$(".shake",this).animate({paddingLeft:"18px"},100,function(){
+			$(".shake",this).animate({paddingLeft:"8px"},100)
+		})
+		$(".shake",this).animate({paddingLeft:"8px"},100,function(){
+			$(".shake",this).animate({paddingLeft:"18px"},100)
+		})
+	},function(){
+		$(".shake",this).stop(true).animate({paddingLeft:"8px"},130)
+	})
+	$(".animate1").hover(function(){
+		$(".shake",this).animate({paddingLeft:"18px"},100,function(){
+			$(".shake",this).animate({paddingLeft:"8px"},100)
+		})
+		$(".shake",this).animate({paddingLeft:"8px"},100,function(){
+			$(".shake",this).animate({paddingLeft:"18px"},100)
+		})
+		$(".shake",this).animate({paddingLeft:"18px"},100,function(){
+			$(".shake",this).animate({paddingLeft:"8px"},100)
+		})
+		$(".shake",this).animate({paddingLeft:"8px"},100,function(){
+			$(".shake",this).animate({paddingLeft:"18px"},100)
+		})
+		$(".shake",this).animate({paddingLeft:"18px"},100,function(){
+			$(".shake",this).animate({paddingLeft:"8px"},100)
+		})
+		$(".shake",this).animate({paddingLeft:"8px"},100,function(){
+			$(".shake",this).animate({paddingLeft:"18px"},100)
+		})
+	},function(){
+		$(".shake",this).stop(true).animate({paddingLeft:"8px"},130)
+	})
+	
+	// 弹出框
+	$(".guanbi").click(function(){
+		$(".popup").addClass("shrink-a")
+		$(this).parents(".popup").addClass("shrink-c")
+		$(this).parents(".popup-center").addClass("shrink");
+		$(this).prevAll().addClass("shrink-a");
+		if($(this).click){
+			setTimeout(function(){
+				autoPlay()
+			},60000)
+		}
+	})
+	
+	var t;
+	function autoPlay(){
+		$(".popup").addClass("shrink-b")
+		$(".guanbi").parents(".popup").removeClass("shrink-c")
+		$(".popup-center").removeClass("shrink");
+		$(".guanbi").prevAll().removeClass("shrink-a");
+	}
+	setTimeout(function(){
+		autoPlay()
+	},60000)
+})
